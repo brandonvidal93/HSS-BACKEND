062470a8afdf9302e9bda32397f0e670f763bd2f
brandonvidal578 <brandon.vidal578@pascualbravo.edu.co>
2025-11-19 00:44:32 -0500

    fix(Pastor): add missing newline at end of file

--- src/domain/entities/Pastor.ts
+++ src/domain/entities/Pastor.ts
@@ -8,5 +8,6 @@ export interface Pastor {
   fechaOrdenacion: Date;
   licenciaMinisterial: string;
   estado: 'ACTIVO' | 'JUBILADO' | 'SANCIONADO' | 'EN_PROCESO';
+  // Relación con el templo al que pertenece el pastor
   temploId: string;
 }
